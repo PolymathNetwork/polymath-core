c2f322de6629f8ad9c1425c3042febf5e538e9eb
Adam Dossa <adam.dossa@gmail.com>
2018-03-13 15:14:41 +0000

    Better spacing

--- contracts/modules/STO/DummySTOFactory.sol
+++ contracts/modules/STO/DummySTOFactory.sol
@@ -6,11 +6,11 @@ import '../../interfaces/IModuleFactory.sol';
 contract DummySTOFactory is IModuleFactory {
 
   function deploy(address _owner, bytes _data) external returns(address) {
-    return address(new DummySTO(_owner, msg.sender));
+      return address(new DummySTO(_owner, msg.sender));
   }
 
   function getCost() external returns(uint256) {
-    return 0;
+      return 0;
   }
 
   function getType() external returns(uint8) {
@@ -18,7 +18,7 @@ contract DummySTOFactory is IModuleFactory {
   }
 
   function getName() external returns(bytes32) {
-    return "DummySTO";
+      return "DummySTO";
   }
 
 

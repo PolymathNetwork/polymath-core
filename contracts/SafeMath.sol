6d3588794e16a712b518177585a49ac57a99da50
satyam <satyam@secureblocks.io>
2018-03-14 16:53:02 +0530

    basic functional setup for the registrars

---
+++ contracts/SafeMath.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.18;
+
+/**
+ *  SafeMath <https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/contracts/math/SafeMath.sol/>
+ *  Copyright (c) 2016 Smart Contract Solutions, Inc.
+ *  Released under the MIT License (MIT)
+ */
+
+/// @title Math operations with safety checks
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+    function max64(uint64 a, uint64 b) internal pure returns (uint64) {
+        return a >= b ? a : b;
+    }
+
+    function min64(uint64 a, uint64 b) internal pure returns (uint64) {
+        return a < b ? a : b;
+    }
+
+    function max256(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min256(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
--- contracts/SecurityTokenRegistrar.sol
+++ contracts/SecurityTokenRegistrar.sol
@@ -1,16 +1,80 @@
 pragma solidity ^0.4.18;
 
+import './SecurityToken.sol';
+import './interfaces/ITokenRegistrar';
+
 contract SecurityTokenRegistrar {
 
+    address public moduleRegistry;
+    address public tickerRegistrar;
+
+    struct SecurityTokenData {
+      string symbol;
+      address owner;
+      bytes32 securityDetails;
+    }
+
+    mapping(address => SecurityTokenData) public securityTokens;
+    mapping(string => address) tickers;
+
+    event LogNewSecurityToken(string _ticker, address _securityTokenAddress, address _owner);
+
+     /**
+     * @dev Constructor use to set the essentials addresses to facilitate
+     * the creation of the security token
+     */
+    function SecurityTokenRegistrar(address _moduleRegistry, address _tickerRegistrar) public {
+        moduleRegistry = _moduleRegistry;
+        tickerRegistrar = _tickerRegistrar;
+    }
+
+    /**
+     * @dev Creates a new Security Token and saves it to the registry
+     * @param _owner Ethereum public key address of the security token owner
+     * @param _name Name of the security token
+     * @param _symbol Ticker name of the security
+     * @param _decimals Decimals value for token
+     * @param _securityDetails off-chain security details
+     */
+    function generateSecurityToken(address _owner, string _name, string _symbol, uint8 _decimals, bytes32 _securityDetails) public {
+        require(_owner != address(0));
+        require(bytes(_name).length > 0 && bytes(_symbol).length > 0);
+        ITokenRegistrar(tickerRegistrar).checkValidity(_symbol,_owner);
+        address newSecurityTokenAddress = new SecurityToken(_owner, _name, _symbol, _decimals, _securityDetails, _moduleRegistry);
+        securityTokens[newSecurityTokenAddress] = SecurityTokenData(_symbol, _owner, _securityDetails);
+        tickers[_symbol] = newSecurityTokenAddress;
+        LogNewSecurityToken(_nameSpaceName, _ticker, newSecurityTokenAddress, _owner, _type);
+    }
+
     function attachTokenTransferModule() internal {
-
+        // TODO : Yet decided
     }
 
-    function SecurityTokenRegistrar() public {
-
+    //////////////////////////////
+    ///////// Get Functions
+    //////////////////////////////
+    /**
+     * @dev Get security token address by ticker name
+     * @param _ticker Symbol of the Scurity token
+     * @return address _ticker
+     */
+    function getSecurityTokenAddress(string _ticker) public view returns (address) {
+      return tickers[_ticker];
     }
 
-    function generateSecurityToken() public {
-        
+    /**
+     * @dev Get Security token details by its ethereum address
+     * @param _STAddress Security token address
+     */
+    function getSecurityTokenData(address _STAddress) public view returns (
+      string,
+      address,
+      bytes32
+    ) {
+      return (
+        securityTokens[_STAddress].ticker,
+        securityTokens[_STAddress].owner,
+        securityTokens[_STAddress].securityDetails
+      );
     }
 }

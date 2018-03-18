31a362247bb1cea57458a50543be60cc39d1b534
satyam <satyam@secureblocks.io>
2018-03-18 14:26:14 +0530

    now change the expiry time

--- contracts/interfaces/ITickerRegistrar.sol
+++ contracts/interfaces/ITickerRegistrar.sol
@@ -2,6 +2,17 @@ pragma solidity ^0.4.18;
 
 interface ITickerRegistrar {
 
+    /**
+     * @dev Check the validity of the symbol
+     * @param _symbol token symbol
+     * @param _owner address of the owner
+     */
     function checkValidity(string _symbol, address _owner) public;
+
+     /**
+     * @dev Returns the owner and timestamp for a given symbol
+     * @param _symbol symbol
+     */
+    function getDetails(string _symbol) public view returns (address, uint256, string, bool);
     
 }

91149ee1f4b1cb97ab68704f1f9d64945956619d
satyam <satyam@secureblocks.io>
2018-04-08 22:27:58 +0530

    add test cases for protocol versioning

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,29 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased](https://github.com/PolymathNetwork/polymath-core/compare/npm-publish-2...master)
 
-[__0.3.0__](https://www.npmjs.com/package/polymath-core_v2?activeTab=readme) __02-04-18__
+[__0.3.1__](https://www.npmjs.com/package/polymath-core?activeTab=readme) __06-04-18__
+
+## Added
+* Add `emit` keyword to emit the events.    
+* Two new variable is added at the time of registeration of ticker. `swarmHash` represents the off-chain data storage location on IPFS and `owner` It reperesent the ethereum address of the owner.       
+* `LogRegisterTicker` emits two more variable called `_swarmHash` and `_owner`.   
+* Two events are added in `GeneralPermissionManager` contract to facilitate the notifications for the UI end.
+          __`LogChangePermission`__ :Emit when permissions to a delegate get changed.    
+          __`LogAddPermission`__: Emit when delegate is added in permission manager contract.   
+* `getInstructions()` is a public function added into the factories contracts. Use to get the useful instructions about the corresponding factory.   
+* `_securityTokenRegistry` is more argument is added in __securityTokenV2__ contract. 
+
+## Changed
+* All contracts get migrated from solc version 0.4.18 to 0.4.21. 
+* Now symbols get stored in smart contract in uppercase instead of lowercase.    
+* Public variable `STRAdress` name in TickerRegistry smart contract changed to `strAddress`.   
+* Function `permissions()` name in all module factories get changed to `getPermissions()`.   
+* Function `delegateDetails()` name gets changed to `getDelegateDetails()` in GeneralPermissionManager contract.      
+* `STVersionProxy_001 & STVersionProxy_002` contract name changed to STVersionProxy001 & STVersionProxy002 respectively.   
+
+***
+
+[__0.3.0__](https://www.npmjs.com/package/polymath-core?activeTab=readme) __02-04-18__
 
 ## Added   
 * Multiple events are added to `ModuleRegistry` contract to facilitate the Logging of the operations.   
@@ -24,9 +46,9 @@ All notable changes to this project will be documented in this file.
 * `removeModule` new function added facilitate the removal of the module from the securityToken. It is ownable type function.    
 * `changeModuleBudget` function added to change the budget of the module that already been added. It is ownable type function.  
 
-## Changed
+## Changed 
 * In early release token symbol in uppercase or in lowercase entertain differently. But for this release both upercase and lowercase symbol name are same.   
-* Address of the owner of the securityToken is removed from the strucuture of the `SecurityTokenData`.
+* Address of the owner of the securityToken is removed from the strucuture of the `SecurityTokenData`. 
 * Mapping `securityTokens` in ISecurityTokenRegistry is now being private instead of public.   
 * `expiryLimit` default value get changed to 7 days instead of the 90 days in TickerRegistry.    
 * __contact__ variable is replaced by the __tokenName__ variable in `SymbolDetails` structure of TickerRegistry.  
@@ -39,12 +61,12 @@ All notable changes to this project will be documented in this file.
 * `LogModuleAdded` emit one more variable called ___budget__.   
 * `modules` mapping in the securityToken contract now returns __the array of ModuleData__.    
 
-## Removed
+## Removed 
 * `admin` varaible is removed from the TickerRegistry contract.    
 
 ***
 
-[__0.2.0__](https://www.npmjs.com/package/polymath-core_v2?activeTab=readme) __26-03-18__
+[__0.2.0__](https://www.npmjs.com/package/polymath-core?activeTab=readme) __26-03-18__
 
 ## Added      
 * ModuleRegistry contract will provide the list of modules by there types.  
@@ -53,9 +75,9 @@ All notable changes to this project will be documented in this file.
 * `SecurityToken` now have the integration with polyToken. At the time of `addModule()` SecurityToken approve the cost of the module to moduleFactory as the spender.   
 * New function `withdrawPoly(uint256 _amount)` is added to withdrawal the unused POLY from the securityToken contract. Called only by the owner of the contract.   
 * `checkPermission(address _delegate, address _module, bytes32 _perm)` function is added to check the permissions on the service providers(delegate).
-* `STVersionProxy_001.sol` & `STVersionProxy_002.sol` are the two new contract added. Both of those are the proxy contract use to generate the SecurityToken. Both contract constructor takes two variables address of `transferManagerFactory` address of the
+* `STVersionProxy_001.sol` & `STVersionProxy_002.sol` are the two new contract added. Both of those are the proxy contract use to generate the SecurityToken. Both contract constructor takes two variables address of `transferManagerFactory` address of the 
 `permissionManagerFactory`.   
-* New Module type is added called `PermissionManager`. It has three contracts called GeneralPermissionManagerFactory, GeneralPermissionManager, IPermissionManager.
+* New Module type is added called `PermissionManager`. It has three contracts called GeneralPermissionManagerFactory, GeneralPermissionManager, IPermissionManager. 
 * `GeneralPermissionManger` is all about providing the permission to the delegate corresponds to the SecurityToken. Major functionality is added, check, change the permission of the delegate.   
 * Two more functions added for each factory type i.e `getDescription()` & `getTitle()`.  
 * `CappedSTO` is now Configurable by choosing the type of fundraise. Either POLY or ETH.
@@ -66,7 +88,7 @@ All notable changes to this project will be documented in this file.
 * `permissions()` function added in GeneralTransferManager to get all permissions.  
 * `PolyToken.sol` contract is added at contracts/helpers/PolyToken.sol. For now, it has no big use.
 
-## Changed
+## Changed 
 * ModuleRegistry only holds the module type of modules only not there names or cost anymore.   
 * No More ModuleReputation struct for counting the reputation of module. Now `reputation` mapping only contains the list of the addresses those used that module factory.    
 * `checkModule()` of ModuleRegistry contract rename to `useModule()` with same function parameters.   
@@ -91,3 +113,6 @@ All notable changes to this project will be documented in this file.
 * `SafeMath.sol` contract is replaced by the zeppelin-solidity library contract .  
 *  No more `SecurityTokens` and `symbol` information will be directly part of the SecurityTokenRegistry. Those information will accessed by inheriting the `ISecurityTokenRegistry`.   
 * Remove the Delegable.sol, AclHelpers.sol, DelegablePorting.sol contracts. Now permission manager factory takes their place . * `delegates` mapping removed from the GeneralTransferManager.  
+
+
+
---
+++ test/security_token_registry.js
@@ -0,0 +1,336 @@
+import latestTime from './helpers/latestTime';
+import { duration, ensureException } from './helpers/utils';
+import takeSnapshot, { increaseTime, revertToSnapshot } from './helpers/time';
+
+const CappedSTOFactory = artifacts.require('./CappedSTOFactory.sol');
+const CappedSTO = artifacts.require('./CappedSTO.sol');
+const ModuleRegistry = artifacts.require('./ModuleRegistry.sol');
+const SecurityToken = artifacts.require('./SecurityToken.sol');
+const SecurityTokenRegistry = artifacts.require('./SecurityTokenRegistry.sol');
+const TickerRegistry = artifacts.require('./TickerRegistry.sol');
+const STVersion = artifacts.require('./STVersionProxy001.sol');
+const STVersion002 = artifacts.require('./STVersionProxy002.sol');
+const GeneralPermissionManagerFactory = artifacts.require('./GeneralPermissionManagerFactory.sol');
+const GeneralTransferManagerFactory = artifacts.require('./GeneralTransferManagerFactory.sol');
+const GeneralTransferManager = artifacts.require('./GeneralTransferManager');
+const GeneralPermissionManager = artifacts.require('./GeneralPermissionManager');
+const PolyToken = artifacts.require('./PolyToken.sol');
+const PolyTokenFaucet = artifacts.require('./helpers/contracts/PolyTokenFaucet.sol');
+
+const Web3 = require('web3');
+const BigNumber = require('bignumber.js');
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")) // Hardcoded development port
+
+
+contract('SecurityTokenRegistry', accounts => {
+
+
+    // Accounts Variable declaration
+    let account_polymath;
+    let account_investor1;
+    let account_issuer;
+    let token_owner;
+    let account_investor2;
+    let account_fundsReceiver;
+    let account_delegate;
+    let account_temp;
+
+    let balanceOfReceiver;
+    // investor Details
+    let fromTime = latestTime();
+    let toTime = latestTime() + duration.days(100);
+    
+    let ID_snap;
+    const message = "Transaction Should Fail!!";
+
+    // Contract Instance Declaration
+    let I_GeneralPermissionManagerFactory;
+    let I_GeneralTransferManagerFactory;
+    let I_GeneralPermissionManager;
+    let I_GeneralTransferManager;
+    let I_ModuleRegistry;
+    let I_TickerRegistry;
+    let I_SecurityTokenRegistry;
+    let I_CappedSTOFactory;
+    let I_STVersion;
+    let I_SecurityToken;
+    let I_CappedSTO;
+    let I_PolyToken;
+    let I_PolyFaucet;
+    let I_STVersion002;
+    let I_SecurityToken002
+    // SecurityToken Details (Launched ST on the behalf of the issuer)
+    const swarmHash = "dagwrgwgvwergwrvwrg";
+    const name = "Demo Token";
+    const symbol = "DET";
+    const tokenDetails = "This is equity type of issuance";
+    const decimals = 18;
+
+    //Security Token Detials (Version 2)
+    const swarmHash2 = "dagwrgwgvwergwrvwrg";
+    const name2 = "Demo2 Token";
+    const symbol2 = "DET2";
+    const tokenDetails2 = "This is equity type of issuance";
+    
+    // Module key
+    const permissionManagerKey = 1;
+    const transferManagerKey = 2;
+    const stoKey = 3;
+    const budget = 0;
+
+    
+
+    before(async() => {
+        // Accounts setup
+        account_polymath = accounts[0];
+        account_issuer = accounts[1];
+        account_investor1 = accounts[9];
+        account_investor2 = accounts[6];
+        account_fundsReceiver = accounts[4];
+        account_delegate = accounts[5];
+        account_temp = accounts[8];
+        token_owner = account_issuer;
+      
+        // ----------- POLYMATH NETWORK Configuration ------------
+
+        // STEP 1: Deploy the ModuleRegistry
+
+        I_ModuleRegistry = await ModuleRegistry.new({from:account_polymath});
+
+        assert.notEqual(
+            I_ModuleRegistry.address.valueOf(),
+            "0x0000000000000000000000000000000000000000",
+            "ModuleRegistry contract was not deployed"
+        );
+
+        // STEP 2: Deploy the GeneralTransferManagerFactory
+
+        I_GeneralTransferManagerFactory = await GeneralTransferManagerFactory.new({from:account_polymath});
+
+        assert.notEqual(
+            I_GeneralTransferManagerFactory.address.valueOf(),
+            "0x0000000000000000000000000000000000000000",
+            "GeneralTransferManagerFactory contract was not deployed"
+        );
+
+        // STEP 3: Deploy the GeneralDelegateManagerFactory
+
+        I_GeneralPermissionManagerFactory = await GeneralPermissionManagerFactory.new({from:account_polymath});
+
+        assert.notEqual(
+            I_GeneralPermissionManagerFactory.address.valueOf(),
+            "0x0000000000000000000000000000000000000000",
+            "GeneralDelegateManagerFactory contract was not deployed"
+        );
+
+        // STEP 4: Deploy the CappedSTOFactory
+
+        I_CappedSTOFactory = await CappedSTOFactory.new({ from: token_owner });
+
+        assert.notEqual(
+            I_CappedSTOFactory.address.valueOf(),
+            "0x0000000000000000000000000000000000000000",
+            "CappedSTOFactory contract was not deployed"
+        );
+
+        // STEP 5: Register the Modules with the ModuleRegistry contract
+
+        // (A) :  Register the GeneralTransferManagerFactory
+        await I_ModuleRegistry.registerModule(I_GeneralTransferManagerFactory.address, { from: account_polymath });
+        await I_ModuleRegistry.verifyModule(I_GeneralTransferManagerFactory.address, true, { from: account_polymath });
+
+        // (B) :  Register the GeneralDelegateManagerFactory
+        await I_ModuleRegistry.registerModule(I_GeneralPermissionManagerFactory.address, { from: account_polymath });
+        await I_ModuleRegistry.verifyModule(I_GeneralPermissionManagerFactory.address, true, { from: account_polymath });
+
+        // (C) : Register the STOFactory
+        await I_ModuleRegistry.registerModule(I_CappedSTOFactory.address, { from: token_owner });
+
+        // Step 6: Deploy the TickerRegistry
+
+        I_TickerRegistry = await TickerRegistry.new({ from: account_polymath });
+
+        assert.notEqual(
+            I_TickerRegistry.address.valueOf(),
+            "0x0000000000000000000000000000000000000000",
+            "TickerRegistry contract was not deployed",
+        );
+
+        // Step 7: Deploy the STversionProxy contract
+
+        I_STVersion = await STVersion.new(I_GeneralTransferManagerFactory.address, I_GeneralPermissionManagerFactory.address, {from : account_polymath });
+
+        assert.notEqual(
+            I_STVersion.address.valueOf(),
+            "0x0000000000000000000000000000000000000000",
+            "STVersion contract was not deployed",
+        );
+
+        // Step ANY: Deploy the Polytoken Contract
+         I_PolyToken = await PolyToken.new();
+
+        // Step 8: Deploy the SecurityTokenRegistry
+
+        I_SecurityTokenRegistry = await SecurityTokenRegistry.new(
+            I_PolyToken.address,
+            I_ModuleRegistry.address,
+            I_TickerRegistry.address,
+            I_STVersion.address,
+            {
+                from: account_polymath
+            });
+
+        assert.notEqual(
+            I_SecurityTokenRegistry.address.valueOf(),
+            "0x0000000000000000000000000000000000000000",
+            "SecurityTokenRegistry contract was not deployed",
+        );
+
+        // Step 8: Set the STR in TickerRegistry
+        await I_TickerRegistry.setTokenRegistry(I_SecurityTokenRegistry.address, {from: account_polymath});
+        await I_ModuleRegistry.setTokenRegistry(I_SecurityTokenRegistry.address, {from: account_polymath});
+
+        // Step 9: Deploy the token Faucet
+        I_PolyFaucet = await PolyTokenFaucet.new();
+
+        // Printing all the contract addresses
+        console.log(`\nPolymath Network Smart Contracts Deployed:\n
+            ModuleRegistry: ${I_ModuleRegistry.address}\n
+            GeneralTransferManagerFactory: ${I_GeneralTransferManagerFactory.address}\n
+            GeneralPermissionManagerFactory: ${I_GeneralPermissionManagerFactory.address}\n
+            CappedSTOFactory: ${I_CappedSTOFactory.address}\n
+            TickerRegistry: ${I_TickerRegistry.address}\n
+            STVersionProxy_001: ${I_STVersion.address}\n
+            SecurityTokenRegistry: ${I_SecurityTokenRegistry.address}\n
+        `);
+    });
+
+    describe("Generate the SecurityToken", async() => {
+
+        it("Should register the ticker before the generation of the security token", async () => {
+            let tx = await I_TickerRegistry.registerTicker(token_owner, symbol, name, swarmHash, { from : token_owner });
+            assert.equal(tx.logs[0].args._owner, token_owner);
+            assert.equal(tx.logs[0].args._symbol, symbol);
+        });
+
+        it("Should generate the new security token with the same symbol as registered above", async () => {
+            let tx = await I_SecurityTokenRegistry.generateSecurityToken(name, symbol, decimals, tokenDetails, { from: token_owner });
+
+            // Verify the successful generation of the security token
+            assert.equal(tx.logs[1].args._ticker, symbol, "SecurityToken doesn't get deployed");
+
+            I_SecurityToken = SecurityToken.at(tx.logs[1].args._securityTokenAddress);
+
+            const LogAddModule = await I_SecurityToken.allEvents();
+            const log = await new Promise(function(resolve, reject) {
+                LogAddModule.watch(function(error, log){ resolve(log);});
+            });
+
+            // Verify that GeneralPermissionManager module get added successfully or not
+            assert.equal(log.args._type.toNumber(), permissionManagerKey);
+            assert.equal(
+                web3.utils.toAscii(log.args._name)
+                .replace(/\u0000/g, ''),
+                "GeneralPermissionManager"
+            );
+            LogAddModule.stopWatching();
+        });
+
+        it("Should deploy the st vesrion 2", async() => {
+            // Step 7: Deploy the STversionProxy contract
+
+            I_STVersion002 = await STVersion002.new(I_GeneralTransferManagerFactory.address, I_GeneralPermissionManagerFactory.address, {from : account_polymath });
+
+            assert.notEqual(
+                I_STVersion002.address.valueOf(),
+                "0x0000000000000000000000000000000000000000",
+                "STVersion002 contract was not deployed",
+            );
+            I_SecurityTokenRegistry.setProtocolVersion(I_STVersion002.address, "0.2.0", { from: account_polymath });
+
+            assert.equal(
+                web3.utils.toAscii(await I_SecurityTokenRegistry.protocolVersion.call())
+                .replace(/\u0000/g, ''),
+                "0.2.0"
+            );
+        });
+
+        it("Should register the ticker before the generation of the security token", async () => {
+            let tx = await I_TickerRegistry.registerTicker(token_owner, symbol2, name2, swarmHash, { from : token_owner });
+            assert.equal(tx.logs[0].args._owner, token_owner);
+            assert.equal(tx.logs[0].args._symbol, symbol2);
+        });
+
+        it("Should generate the new security token with version 2", async() => {
+            let tx = await I_SecurityTokenRegistry.generateSecurityToken(name2, symbol2, decimals, tokenDetails, { from: token_owner });
+
+            // Verify the successful generation of the security token
+            assert.equal(tx.logs[1].args._ticker, symbol2, "SecurityToken doesn't get deployed");
+
+            I_SecurityToken002 = SecurityToken.at(tx.logs[1].args._securityTokenAddress);
+
+            const LogAddModule = await I_SecurityToken002.allEvents();
+            const log = await new Promise(function(resolve, reject) {
+                LogAddModule.watch(function(error, log){ resolve(log);});
+            });
+
+            // Verify that GeneralPermissionManager module get added successfully or not
+            assert.equal(log.args._type.toNumber(), permissionManagerKey);
+            assert.equal(
+                web3.utils.toAscii(log.args._name)
+                .replace(/\u0000/g, ''),
+                "GeneralPermissionManager"
+            );
+            LogAddModule.stopWatching();
+        });
+
+        it("Should deploy the st vesrion 3", async() => {
+            // Step 7: Deploy the STversionProxy contract
+
+            I_STVersion002 = await STVersion002.new(I_GeneralTransferManagerFactory.address, I_GeneralPermissionManagerFactory.address, {from : account_polymath });
+
+            assert.notEqual(
+                I_STVersion002.address.valueOf(),
+                "0x0000000000000000000000000000000000000000",
+                "STVersion002 contract was not deployed",
+            );
+            I_SecurityTokenRegistry.setProtocolVersion(I_STVersion002.address, "0.3.0", { from: account_polymath });
+
+            assert.equal(
+                web3.utils.toAscii(await I_SecurityTokenRegistry.protocolVersion.call())
+                .replace(/\u0000/g, ''),
+                "0.3.0"
+            );
+        });
+
+        it("Should register the ticker before the generation of the security token", async () => {
+            let tx = await I_TickerRegistry.registerTicker(token_owner, "DET3", name2, swarmHash, { from : token_owner });
+            assert.equal(tx.logs[0].args._owner, token_owner);
+            assert.equal(tx.logs[0].args._symbol, "DET3");
+        });
+
+        it("Should generate the new security token with version 3", async() => {
+            let tx = await I_SecurityTokenRegistry.generateSecurityToken(name2, "DET3", decimals, tokenDetails, { from: token_owner });
+
+            // Verify the successful generation of the security token
+            assert.equal(tx.logs[1].args._ticker, "DET3", "SecurityToken doesn't get deployed");
+
+            I_SecurityToken002 = SecurityToken.at(tx.logs[1].args._securityTokenAddress);
+
+            const LogAddModule = await I_SecurityToken002.allEvents();
+            const log = await new Promise(function(resolve, reject) {
+                LogAddModule.watch(function(error, log){ resolve(log);});
+            });
+
+            // Verify that GeneralPermissionManager module get added successfully or not
+            assert.equal(log.args._type.toNumber(), permissionManagerKey);
+            assert.equal(
+                web3.utils.toAscii(log.args._name)
+                .replace(/\u0000/g, ''),
+                "GeneralPermissionManager"
+            );
+            LogAddModule.stopWatching();
+        });
+
+    });
+});

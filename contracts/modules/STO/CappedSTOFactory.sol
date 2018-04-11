fc822a43f3e0513c1ff9612b83431a5035286ef9
satyam <satyam@secureblocks.io>
2018-04-11 14:30:00 +0530

    transferFrom calls gets wrapped in require

--- contracts/modules/STO/CappedSTOFactory.sol
+++ contracts/modules/STO/CappedSTOFactory.sol
@@ -14,7 +14,7 @@ contract CappedSTOFactory is IModuleFactory {
     }
 
     function deploy(bytes _data) external returns(address) {
-        polyToken.transferFrom(msg.sender, owner, getCost());
+        require(polyToken.transferFrom(msg.sender, owner, getCost()));
         //Check valid bytes - can only call module init function
         CappedSTO cappedSTO = new CappedSTO(msg.sender);
         //Checks that _data is valid (not calling anything it shouldn't)
--- contracts/modules/STO/DummySTOFactory.sol
+++ contracts/modules/STO/DummySTOFactory.sol
@@ -15,7 +15,7 @@ contract DummySTOFactory is IModuleFactory {
 
 
     function deploy(bytes _data) external returns(address) {
-        polyToken.transferFrom(msg.sender, owner, getCost());
+        require(polyToken.transferFrom(msg.sender, owner, getCost()));
         //Check valid bytes - can only call module init function
         DummySTO dummySTO = new DummySTO(msg.sender);
         //Checks that _data is valid (not calling anything it shouldn't)

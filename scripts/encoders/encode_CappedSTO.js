478184af8b79c29d8426e90c74e4384ce06c9d6c
Pablo Ruiz <pabloruiz55@Pablos-MacBook-Pro.local>
2018-04-16 14:49:06 -0300

    added CappedSTO encoding script

---
+++ scripts/encoders/encode_CappedSTO.js
@@ -0,0 +1,45 @@
+const Web3 = require('web3');
+
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  // set the provider you want from Web3.providers
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+let startTime = process.argv.slice(2)[0];
+let endTime = process.argv.slice(2)[1];
+let cap = process.argv.slice(2)[2];
+let rate = process.argv.slice(2)[3];
+let wallet = process.argv.slice(2)[4];
+
+
+let bytesSTO = web3.eth.abi.encodeFunctionCall({
+        name: 'configure',
+        type: 'function',
+        inputs: [{
+            type: 'uint256',
+            name: '_startTime'
+        },{
+            type: 'uint256',
+            name: '_endTime'
+        },{
+            type: 'uint256',
+            name: '_cap'
+        },{
+            type: 'uint256',
+            name: '_rate'
+        },{
+            type: 'uint8',
+            name: '_fundRaiseType'
+        },{
+            type: 'address',
+            name: '_polyToken'
+        },{
+            type: 'address',
+            name: '_fundsReceiver'
+        }
+        ]
+    }, [startTime, endTime, web3.utils.toWei(cap, 'ether'), rate,0,0,wallet]);
+
+  console.log(bytesSTO);

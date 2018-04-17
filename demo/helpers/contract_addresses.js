ef40c5b1826841e24a19404a0e2f6a48c5242b1f
Pablo Ruiz <pabloruiz55@Pablos-MacBook-Pro.local>
2018-04-17 09:43:35 -0300

    only transfer POLY if factory cost is >0

--- demo/helpers/contract_addresses.js
+++ demo/helpers/contract_addresses.js
@@ -3,24 +3,24 @@ var NETWORKS = {
   MAINNET: 1,
   ROPSTEN: 3,
 };
-var SELECTED_NETWORK = NETWORKS.ROPSTEN;
+var SELECTED_NETWORK = NETWORKS.GANACHE;
 
 module.exports = {
   tickerRegistryAddress: function() {
     if(SELECTED_NETWORK == NETWORKS.GANACHE)
-      return "0x9c6d96643165bb6d095a6558e8c5f382e510ae89";
+      return "0x8942f67e0ab0a7bf32a8c975c3eca3efb4cc1164";
     else if(SELECTED_NETWORK == NETWORKS.ROPSTEN)
       return "0x0abeafae9333f7dc64959914f67765edee5fd05f";
   },
   securityTokenRegistryAddress: function() {
     if(SELECTED_NETWORK == NETWORKS.GANACHE)
-      return "0xe2789abc621d39817bccf7507296bba536c5cc1d";
+      return "0x720d6edf211fff831537514c3aa6ce3ea882e8e0";
     else if(SELECTED_NETWORK == NETWORKS.ROPSTEN)
       return "0x8e736d9caeb9eb9b7a766c196fbb01eb53344e81";
   },
   cappedSTOFactoryAddress: function() {
     if(SELECTED_NETWORK == NETWORKS.GANACHE)
-      return "0x96d63dd0093d1a47dd94e92c612cb0f4c3b8280a";
+      return "0xdd398df235815e879e661d9f129eba83d4a2ea74";
     else if(SELECTED_NETWORK == NETWORKS.ROPSTEN)
       return "0x423105526255db03c36fd763c982dad96b95d17e";
   },
--- test/helpers/contracts/TestSTOFactory.sol
+++ test/helpers/contracts/TestSTOFactory.sol
@@ -14,7 +14,8 @@ contract TestSTOFactory is IModuleFactory {
     }
 
     function deploy(bytes _data) external returns(address) {
-        require(polyToken.transferFrom(msg.sender, owner, getCost()));
+        if(getCost() >0)
+            require(polyToken.transferFrom(msg.sender, owner, getCost()));
         //Check valid bytes - can only call module init function
         DummySTO dummySTO = new DummySTO(msg.sender);
         //Checks that _data is valid (not calling anything it shouldn't)

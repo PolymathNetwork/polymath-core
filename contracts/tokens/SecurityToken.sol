9b07ed9f0727dd231d5ab2a62596a98f11387f17
Adam Dossa <adam.dossa@gmail.com>
2018-05-23 10:13:00 +0100

    Adjust investor count with burn

--- contracts/tokens/SecurityToken.sol
+++ contracts/tokens/SecurityToken.sol
@@ -247,7 +247,7 @@ contract SecurityToken is ISecurityToken {
             investorCount = investorCount.sub(1);
         }
         // Check whether receiver is a new token holder
-        if (balanceOf(_to) == 0) {
+        if ((balanceOf(_to) == 0) && (_to != address(0))) {
             investorCount = investorCount.add(1);
         }
 
@@ -347,6 +347,7 @@ contract SecurityToken is ISecurityToken {
     }
 
     function burn(uint256 _value) checkGranularity(_value) public {
+        adjustInvestorCount(msg.sender, address(0), _value);
         require(tokenBurner != address(0), "Token Burner contract address is not set yet");
         require(_value <= balances[msg.sender], "Value should no be greater than the balance of msg.sender");
         // no need to require value <= totalSupply, since that would imply the

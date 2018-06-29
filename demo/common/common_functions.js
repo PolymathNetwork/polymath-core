e7e25fb0103bc9827b3c709c6f48866dba02283d
Victor Vicente <victor.g.vicente@gmail.com>
2018-06-29 09:02:57 -0300

    Added value to estimageGas

--- demo/common/common_functions.js
+++ demo/common/common_functions.js
@@ -1,8 +1,8 @@
 var chalk = require('chalk');
 
 module.exports = {
-  estimateGas: async function (actionToEstimate, fromAddress, factor) {
-    let estimatedGAS = Math.round(factor * (await actionToEstimate.estimateGas({ from: fromAddress })));
+  estimateGas: async function (actionToEstimate, fromAddress, factor, value) {
+    let estimatedGAS = Math.round(factor * (await actionToEstimate.estimateGas({ from: fromAddress, value: value})));
     console.log(chalk.black.bgYellowBright(`---- Transaction executed: ${actionToEstimate._method.name} - Gas limit provided: ${estimatedGAS} ----`));    
     return estimatedGAS;
   }

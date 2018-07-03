25c2f1bb62574fd2b877a0394bda239082d49bd7
satyam <satyam@secureblocks.io>
2018-07-03 20:57:00 +0530

    travis and test case fixes

--- test/ticker_registry.js
+++ test/ticker_registry.js
@@ -453,11 +453,11 @@ contract('TickerRegistry', accounts => {
         describe("Test cases for reclaiming funds", async() => {
 
             it("Should successfully reclaim POLY tokens", async() => {
-                I_PolyToken.transfer(I_TickerRegistry.address, 1 * Math.pow(10, 18), { from: token_owner });
+                I_PolyToken.transfer(I_TickerRegistry.address, web3.utils.toWei("1"), { from: token_owner });
                 let bal1 = await I_PolyToken.balanceOf.call(account_polymath);
                 await I_TickerRegistry.reclaimERC20(I_PolyToken.address);
                 let bal2 = await I_PolyToken.balanceOf.call(account_polymath);
-                assert.isAbove(bal2, bal1);
+                assert.isAtLeast(bal2.dividedBy(new BigNumber(10).pow(18)).toNumber(), bal2.dividedBy(new BigNumber(10).pow(18)).toNumber());
             });
 
             it("Should fail to reclaim tokens if address provided is 0 address", async() => {

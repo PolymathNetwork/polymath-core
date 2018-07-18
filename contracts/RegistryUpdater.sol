a66764cd3560a20acbb2db5bba44be41cff4a4aa
satyam <satyam@secureblocks.io>
2018-07-18 18:58:58 +0530

    add require check in constructor

--- contracts/RegistryUpdater.sol
+++ contracts/RegistryUpdater.sol
@@ -12,11 +12,11 @@ contract RegistryUpdater is Ownable {
     address public polyToken;
 
     constructor (address _polymathRegistry) public {
+        require(_polymathRegistry != address(0));
         polymathRegistry = _polymathRegistry;
     }
 
     function updateFromRegistry() onlyOwner public {
-        require(polymathRegistry != address(0));
         moduleRegistry = PolymathRegistry(polymathRegistry).getAddress("ModuleRegistry");
         securityTokenRegistry = PolymathRegistry(polymathRegistry).getAddress("SecurityTokenRegistry");
         tickerRegistry = PolymathRegistry(polymathRegistry).getAddress("TickerRegistry");

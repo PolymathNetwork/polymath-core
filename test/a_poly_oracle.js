937dbb39c0fcb06f0098ca2e749ddc6911c16b85
Adam Dossa <adam.dossa@gmail.com>
2018-08-08 23:29:38 +0100

    Fix test cases

--- test/a_poly_oracle.js
+++ test/a_poly_oracle.js
@@ -17,6 +17,7 @@ const SanityBounds = 20*10**16;
 const GasLimit = 100000;
 const TimeTolerance = 5*60;
 const message = "Txn should fail";
+let latestPrice;
 let requestIds = new Array();
 
     before(async()=> {
@@ -26,7 +27,7 @@ let requestIds = new Array();
 
 
     describe("state variables checks", async() => {
-        
+
         it("should check the api url", async() => {
             let url = await I_PolyOracle.oracleURL.call();
             assert.equal(URL, url);
@@ -51,7 +52,7 @@ let requestIds = new Array();
 
     describe("Scheduling test cases", async() => {
 
-        it("Should schedule the timing of the call", async() => {
+        it("Should schedule the timing of the call - fails - non owner", async() => {
             let errorThrown = false;
             let timeScheduling = [latestTime()+duration.minutes(1), latestTime()+duration.minutes(2), latestTime()+duration.minutes(3)]
             try {
@@ -61,10 +62,10 @@ let requestIds = new Array();
                 ensureException(error);
                 console.log(`       tx -> revert msg.sender should be the owner of the contract`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         });
 
-        it("Should schedule the timing of the call", async() => {
+        it("Should schedule the timing of the call - fails - no value", async() => {
             let errorThrown = false;
             let timeScheduling = [latestTime()+duration.minutes(1), latestTime()+duration.minutes(2), latestTime()+duration.minutes(3)]
             try {
@@ -74,30 +75,31 @@ let requestIds = new Array();
                 ensureException(error);
                 console.log(`       tx -> revert Because value for txn is not provided`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         })
 
-        it("Should schedule the timing of the call", async() => {
+        it("Should schedule the timing of the call - single call", async() => {
+            let blockNo = await web3.eth.getBlock('latest').number;
             await I_PolyOracle.schedulePriceUpdatesFixed([],{from: owner, value:web3.utils.toWei("1")});
 
             let log = await assertEvent(I_PolyOracle, {event: 'LogNewOraclizeQuery',logIndex: 1});
 
             assert.isAtMost(log[0].args._time.toNumber(), latestTime());
-            await increaseTime(50);
+            // await increaseTime(50);
+            const logNewPriceWatcher = await promisifyLogWatch(I_PolyOracle.LogPriceUpdated({ fromBlock: blockNo }), 1);
+            // const log = await logNewPriceWatcher;
+            assert.equal(logNewPriceWatcher.event, 'LogPriceUpdated', 'LogPriceUpdated not emitted.')
+            assert.isNotNull(logNewPriceWatcher.args._price, 'Price returned was null.')
+            assert.equal(logNewPriceWatcher.args._oldPrice.toNumber(), 0);
+            console.log('Success! Current price is: ' + logNewPriceWatcher.args._price.dividedBy(new BigNumber(10).pow(18)).toNumber() + ' USD/POLY')
 
-            const LogPriceUpdated = await I_PolyOracle.allEvents();
-            const priceLog = await new Promise(function(resolve, reject) {
-            LogPriceUpdated.watch(function(error, log){ resolve(log);});
-            });
-            assert.equal(priceLog.args._oldPrice.toNumber(), 0);
-            console.log(`       latest price: ${parseFloat(priceLog.args._price.dividedBy(new BigNumber(10).pow(18)).toNumber())}`);
         });
 
-        it("Should schedule the timing of the call", async() => {
+        it("Should schedule the timing of the call - multiple calls", async() => {
             let blockNo = await web3.eth.getBlock('latest').number;
-            let timeScheduling = [latestTime()+duration.seconds(30), latestTime()+duration.seconds(60), latestTime()+duration.seconds(90)]
+            let timeScheduling = [latestTime()+duration.seconds(10), latestTime()+duration.seconds(20)]
             await I_PolyOracle.schedulePriceUpdatesFixed(timeScheduling, {from: owner, value:web3.utils.toWei("1.5")});
-            
+
             let event_data = await assertEvent(I_PolyOracle, {event: 'LogNewOraclizeQuery'});
 
             for (var i = 0; i < event_data.length; i++) {
@@ -105,31 +107,29 @@ let requestIds = new Array();
                 console.log(`       checking the time for the ${i} index and the scheduling time is ${time}`);
                 assert.isAtMost(time.toNumber(), timeScheduling[i]);
             }
-        });
-           
-        it("Should return the callback call", async() => {
-            await latestTime(20);
+
             // Wait for the callback to be invoked by oraclize and the event to be emitted
-            const logNewPriceWatcher = promisifyLogWatch(I_PolyOracle.LogPriceUpdated({ fromBlock: 'latest' }));
+            const logNewPriceWatcher = promisifyLogWatch(I_PolyOracle.LogPriceUpdated({ fromBlock: blockNo }), 2);
             const log = await logNewPriceWatcher;
             assert.equal(log.event, 'LogPriceUpdated', 'LogPriceUpdated not emitted.')
-            assert.isNotNull(log.args._price, 'Price returned was null.')
+            assert.isNotNull(log.args._price, 'Price returned was null.');
             console.log('Success! Current price is: ' + log.args._price.dividedBy(new BigNumber(10).pow(18)).toNumber() + ' USD/POLY')
         });
 
-        it("Should schedule to call using iters", async() => {
+        it("Should schedule to call using iters - fails", async() => {
             let errorThrown = false;
             try {
-                await I_PolyOracle.schedulePriceUpdatesRolling(latestTime(), 30, 5, {from: accounts[6]});
+                await I_PolyOracle.schedulePriceUpdatesRolling(latestTime(), 30, 2, {from: accounts[6]});
             } catch(error) {
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not the owner`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         })
 
         it("Should schedule to call using iters", async() => {
-            await I_PolyOracle.schedulePriceUpdatesRolling(latestTime(), 30, 5, {from: owner});
+            let blockNo = await web3.eth.getBlock('latest').number;
+            await I_PolyOracle.schedulePriceUpdatesRolling(latestTime(), 10, 2, {from: owner});
             let event_data = await assertEvent(I_PolyOracle, {event: 'LogNewOraclizeQuery'});
 
             for (var i = 0; i < event_data.length; i++) {
@@ -138,26 +138,33 @@ let requestIds = new Array();
                 console.log(`       checking the time for the ${i} index and the scheduling time is ${time}`);
                 assert.isAtMost(time.toNumber(), latestTime() + ((i + 1) * 30));
             }
+            // Wait for the callback to be invoked by oraclize and the event to be emitted
+            const logNewPriceWatcher = promisifyLogWatch(I_PolyOracle.LogPriceUpdated({ fromBlock: blockNo }), 2);
+            const log = await logNewPriceWatcher;
+            assert.equal(log.event, 'LogPriceUpdated', 'LogPriceUpdated not emitted.')
+            assert.isNotNull(log.args._price, 'Price returned was null.')
+            console.log('Success! Current price is: ' + log.args._price.dividedBy(new BigNumber(10).pow(18)).toNumber() + ' USD/POLY');
+            latestPrice = log.args._price;
         });
     })
 
     describe("Ownable functions", async() => {
 
-        it("Should change the Poly USD price manually", async() => {
+        it("Should change the Poly USD price manually - fail - bad account", async() => {
             let errorThrown = false;
             try {
-                await I_PolyOracle.setPOLYUSD(web3.utils.toWei("0.4","ether"), {from: accounts[5]});
+                await I_PolyOracle.setPOLYUSD(latestPrice.add(1), {from: accounts[5]});
             } catch(error) {
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not the owner`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         });
 
         it("Should change the Poly USD price manually", async() => {
-            await I_PolyOracle.setPOLYUSD(web3.utils.toWei("0.4","ether"), {from: owner});
+            await I_PolyOracle.setPOLYUSD(latestPrice.add(1), {from: owner});
             let price2 = await I_PolyOracle.getPriceAndTime.call();
-            assert.equal(price2[0], web3.utils.toWei("0.4","ether"))
+            assert.equal(price2[0].toNumber(), latestPrice.add(1).toNumber());
         })
 
         it("Should freeze the Oracle manually", async() => {
@@ -168,7 +175,7 @@ let requestIds = new Array();
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not the owner`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         })
 
         it("Should change the URL manually", async() => {
@@ -180,7 +187,7 @@ let requestIds = new Array();
             await I_PolyOracle.setFreezeOracle(false, {from: owner});
         })
 
-        it("Should change the sanity bounds manually", async() => {
+        it("Should change the sanity bounds manually - fails - bad owner", async() => {
             let errorThrown = false;
             try {
                 await I_PolyOracle.setSanityBounds(new BigNumber(25).times(new BigNumber(10).pow(16)), {from : accounts[6]});
@@ -188,16 +195,18 @@ let requestIds = new Array();
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not the owner`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         })
 
         it("Should change the sanity bounds manually", async() => {
+            console.log(JSON.stringify(await I_PolyOracle.sanityBounds.call()));
             await I_PolyOracle.setSanityBounds(new BigNumber(25).times(new BigNumber(10).pow(16)), {from : owner});
             let sanityBounds = await I_PolyOracle.sanityBounds.call();
+            console.log(JSON.stringify(await I_PolyOracle.sanityBounds.call()));
             assert.equal(sanityBounds.toNumber(), new BigNumber(25).times(new BigNumber(10).pow(16)).toNumber())
         });
 
-        it("Should change the gas price manually", async() => {
+        it("Should change the gas price manually - fails - bad owner", async() => {
             let errorThrown = false;
             try{
                 await I_PolyOracle.setGasPrice(new BigNumber(60).times(new BigNumber(10).pow(9)),{from : accounts[6]});
@@ -205,28 +214,15 @@ let requestIds = new Array();
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not the owner`.grey);
             }
-            assert.ok(message, errorThrown);
-        });
-
-        it("Should change the gas price manually", async() => {
-            let errorThrown = false;
-            await I_PolyOracle.setGasPrice(new BigNumber(80).times(new BigNumber(10).pow(9)),{from : owner});
-            try{
-                let timeScheduling = [latestTime()+duration.minutes(1), latestTime()+duration.minutes(2), latestTime()+duration.minutes(3)]
-                await I_PolyOracle.schedulePriceUpdatesFixed(timeScheduling, {from: owner, value:web3.utils.toWei("1")});
-            } catch(error){
-                errorThrown = true;
-                console.log(`       tx-> gas required exceeds the value provided `.grey);
-            }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         });
 
         it("Should change the gas price manually", async() => {
             await I_PolyOracle.setGasPrice(new BigNumber(60).times(new BigNumber(10).pow(9)),{from : owner});
             let blockNo = await web3.eth.getBlock('latest').number;
-            let timeScheduling = [latestTime()+duration.seconds(30), latestTime()+duration.seconds(60), latestTime()+duration.seconds(90)]
+            let timeScheduling = [latestTime()+duration.seconds(10), latestTime()+duration.seconds(20)];
             await I_PolyOracle.schedulePriceUpdatesFixed(timeScheduling, {from: owner, value:web3.utils.toWei("2")});
-            
+
             let event_data = await assertEvent(I_PolyOracle, {event: 'LogNewOraclizeQuery'});
 
             for (var i = 0; i < event_data.length; i++) {
@@ -234,9 +230,17 @@ let requestIds = new Array();
                 console.log(`       checking the time for the ${i} index and the scheduling time is ${time}`);
                 assert.isAtMost(time.toNumber(), timeScheduling[i]);
             }
+
+            const logNewPriceWatcher = await promisifyLogWatch(I_PolyOracle.LogPriceUpdated({ fromBlock: blockNo }), 2);
+
+            assert.equal(logNewPriceWatcher.event, 'LogPriceUpdated', 'LogPriceUpdated not emitted.')
+            assert.isNotNull(logNewPriceWatcher.args._price, 'Price returned was null.')
+            console.log('Success! Current price is: ' + logNewPriceWatcher.args._price.dividedBy(new BigNumber(10).pow(18)).toNumber() + ' USD/POLY');
+            // assert.isTrue(false);
+
         });
 
-        it("Should change the gas limit manually", async() => {
+        it("Should change the gas limit manually - fails", async() => {
             let errorThrown = false;
             try{
                 await I_PolyOracle.setGasLimit(50000,{from : accounts[6]});
@@ -244,18 +248,19 @@ let requestIds = new Array();
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not owner`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         });
 
         it("Should change the gas limit manually", async() => {
             await I_PolyOracle.setGasLimit(50000,{from : owner});
             let gasLimit = await I_PolyOracle.gasLimit.call();
             assert.equal(gasLimit.toNumber(),50000);
+            await I_PolyOracle.setGasLimit(100000,{from : owner});
         });
 
-        it("Should blacklist some IDS manually", async() => {
+        it("Should blacklist some IDS manually - fails - wrong size", async() => {
             let errorThrown = false;
-            let ignore = [true, false, false];
+            let ignore = [true];
             try{
                 await I_PolyOracle.setIgnoreRequestIds(requestIds,ignore,{from : accounts[6]});
             } catch(error){
@@ -265,7 +270,8 @@ let requestIds = new Array();
         });
 
         it("Should blacklist some IDS manually", async() => {
-            let ignore = [false, false, false, true, true];
+            let ignore = [false, true];
+            console.log(requestIds);
             await I_PolyOracle.setIgnoreRequestIds(requestIds, ignore, {from : owner});
 
             // let ignoreRequestId0 = await I_PolyOracle.ignoreRequestIds.call(requestIds[1]);
@@ -276,7 +282,7 @@ let requestIds = new Array();
 
         });
 
-        it("Should change the oraclize time tolerance manually", async() => {
+        it("Should change the oraclize time tolerance manually - fails", async() => {
             let errorThrown = false;
             try{
                 await I_PolyOracle.setOraclizeTimeTolerance(3600,{from : accounts[6]});
@@ -284,7 +290,7 @@ let requestIds = new Array();
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not the owner`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         })
 
         it("Should change the oraclize time tolerance manually", async() => {
@@ -301,7 +307,7 @@ let requestIds = new Array();
                 errorThrown = true;
                 console.log(`       tx->revert msg.sender is not the owner`.grey);
             }
-            assert.ok(message, errorThrown);
+            assert.ok(errorThrown, message);
         })
 
         it("should change the api URL manually", async() => {
@@ -310,19 +316,16 @@ let requestIds = new Array();
             assert.equal(alternateURL, url);
         });
 
-        it("Should schedule the timing of the call", async() => {
+        it("Should schedule the timing of the call - after changes", async() => {
+            let blockNo = await web3.eth.getBlock('latest').number;
             await I_PolyOracle.schedulePriceUpdatesFixed([],{from: owner, value:web3.utils.toWei("1")});
-
             let log = await assertEvent(I_PolyOracle, {event: 'LogNewOraclizeQuery',logIndex: 1});
-
             assert.isAtMost(log[0].args._time.toNumber(), latestTime());
-            await increaseTime(50);
-
-            const LogPriceUpdated = await I_PolyOracle.allEvents();
-            const priceLog = await new Promise(function(resolve, reject) {
-            LogPriceUpdated.watch(function(error, log){ resolve(log);});
-            });
-            console.log(`       latest price: ${parseFloat(priceLog.args._price.dividedBy(new BigNumber(10).pow(18)).toNumber())}`);
+            const logNewPriceWatcher = await promisifyLogWatch(I_PolyOracle.LogPriceUpdated({ fromBlock: blockNo }), 1);
+            assert.equal(logNewPriceWatcher.event, 'LogPriceUpdated', 'LogPriceUpdated not emitted.')
+            assert.isNotNull(logNewPriceWatcher.args._price, 'Price returned was null.')
+            console.log('Success! Current price is: ' + logNewPriceWatcher.args._price.dividedBy(new BigNumber(10).pow(18)).toNumber() + ' USD/POLY');
+            // assert.isTrue(false);
         });
 
     })
@@ -351,14 +354,18 @@ let requestIds = new Array();
  * Helper to wait for log emission.
  * @param  {Object} _event The event to wait for.
  */
-function promisifyLogWatch(_event) {
+function promisifyLogWatch(_event, _times) {
     return new Promise((resolve, reject) => {
+      let i = 0;
       _event.watch((error, log) => {
-        _event.stopWatching();
         if (error !== null)
           reject(error);
-  
-        resolve(log);
+        i = i + 1;
+        console.log("Received event: " + i + " out of: " + _times);
+        if (i == _times) {
+          _event.stopWatching();
+          resolve(log);
+        }
       });
     });
   }

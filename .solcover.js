f29db7888b602cedf0e87ae51f74ca19c0d6839c
Victor Vicente <victor.g.vicente@gmail.com>
2018-08-30 22:10:16 -0300

    Fix ignore a_poly_oracle.js for coverage

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ module.exports = {
     norpc: true,
     port: 8545,
     copyPackages: ['openzeppelin-solidity'],
-    testCommand: 'node ../node_modules/.bin/truffle test `ls test/*.js --ignore a_poly_oracle.js` --network coverage',
+    testCommand: 'node ../node_modules/.bin/truffle test `find test/*.js ! -name a_poly_oracle.js` --network coverage',
     deepSkip: true,
     skipFiles: ['external', 'flat']
 };

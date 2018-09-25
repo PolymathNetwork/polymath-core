2c1a98c01470d428b17a8c2ed9bcc733e279e56b
pulkit <kpulkit291@gmail.com>
2018-09-25 22:27:45 +0530

    My final implementation for contract

--- contracts/modules/PermissionManager/AutomatedBlackList.sol
+++ contracts/modules/PermissionManager/AutomatedBlackList.sol
@@ -1,13 +1,13 @@
 pragma solidity ^0.4.24;
-import "./GeneralPermissionManager.sol";
+import "../Module.sol";
 import "../TransferManager/ITransferManager.sol";
-
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 /**
  * @title AutomateBlacklist
  * @dev to automate blacklist process
  */
-contract AutomateBlacklist is GeneralPermissionManager,ITransferManager {
-    
+contract AutomateBlacklist is Module,ITransferManager {
+    using SafeMath for uint256;
     /**
      * @dev structure for the blacklist period
      */
@@ -17,25 +17,19 @@ contract AutomateBlacklist is GeneralPermissionManager,ITransferManager {
         uint256 endDate;
         uint256 repeats;
     }
- 
-     /** 
-     *@dev to check if msg.sender can add new entry
-     */
-    modifier isAllowed() {
-        require(msg.sender == securityToken || checkPermission(msg.sender,address(this),"1"));
-        _;
-    }
+    //Flag has to be set
+    bytes32 public constant PERMISSION_FLAG = "FLAG";
   /// use to hold the different blacklist corresponds to name of blacklist type 
     mapping(string => BlacklistDetails) blacklists;
 /// use to hold the blackListed address corresponds to the blacklist type
     mapping(address => string) inverstorToBlacklist;
 /// use to get list of the addresses for a particular blacklist
-    mapping(string => address[]) blacklistToAddress;
+    mapping(string => address[]) _blackListName;
     
     /**
      * @dev to check if transfer can be made
      */
-    function verifyTransfer(address _from, address _to, uint256 _amount, bool _isTransfer) public returns(Result) {
+    function verifyTransfer(address _from, address _to, uint256 _amount, bool _isTransfer) public withPerm(PERMISSION_FLAG)  returns(Result) {
 
         string storage typeOfBlacklist=inverstorToBlacklist[_from];
         require(bytes(inverstorToBlacklist[_from]).length!=0);
@@ -46,19 +40,16 @@ contract AutomateBlacklist is GeneralPermissionManager,ITransferManager {
         if (now > newStart && now < newEnd){ return Result.INVALID;}
         return Result.VALID;
     }
-     /**
-      * @dev to add the new blacklist entry
-      * @param {string} _type
-      * @param {address} _investor
-      * @param {uint256} _start
-      * @param {uint256} _end
-      * @param {uint256} _repeat
-      */
-    function addEntry(string _type,address _investor,uint256 _start,uint256 _end,uint256 _repeat) isAllowed() public {
-        blacklists[_type]=BlacklistDetails(_start,_end,_repeat);
-        inverstorToBlacklist[_investor]=_type;
-        blacklistToAddress[_type].push(_investor);
+ 
+ 
+    function addBlackListType(uint256 _startDate, uint256 _endDate, string _name, uint256 _repeatPeriodInDays) {
+       blacklists[_name]=BlacklistDetails(_startDate,_endDate,_repeatPeriodInDays);
     }
+    function addInvestorToBlackList(address _investor, string _blackListName) {
+       inverstorToBlacklist[_investor]=_blackListName;
+       _blackListName[_blackListName].push(_investor);
+    }
+
  
      
 

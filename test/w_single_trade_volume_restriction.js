149f56dad21c62f1fa71a60d7f205112e43ef44e
Adam Dossa <adam.dossa@gmail.com>
2018-10-04 22:25:00 +0100

    Update w_single_trade_volume_restriction.js

--- test/w_single_trade_volume_restriction.js
+++ test/w_single_trade_volume_restriction.js
@@ -305,7 +305,7 @@ contract('SingleTradeVolumeRestrictionManager', accounts => {
       }), 1);
 
       // Verify that GeneralTransferManager module get added successfully or not
-      assert.equal(log.args._type.toNumber(), 2);
+      assert.equal(log.args._types[0].toNumber(), 2);
       assert.equal(
         web3.utils.toAscii(log.args._name)
         .replace(/\u0000/g, ''),
@@ -314,15 +314,8 @@ contract('SingleTradeVolumeRestrictionManager', accounts => {
     });
 
     it("Should intialize the auto attached modules", async () => {
-      let moduleData = await I_SecurityToken.modules(2, 0);
+      let moduleData = (await I_SecurityToken.getModulesByType(2))[0];
       I_GeneralTransferManager = GeneralTransferManager.at(moduleData);
-
-      assert.notEqual(
-        I_GeneralTransferManager.address.valueOf(),
-        "0x0000000000000000000000000000000000000000",
-        "GeneralTransferManager contract was not deployed",
-      );
-
     });
   });
   //
@@ -431,7 +424,7 @@ contract('SingleTradeVolumeRestrictionManager', accounts => {
       let tx = await I_SecurityToken.addModule(P_SingleTradeVolumeRestrictionManagerFactory.address, managerArgs, web3.utils.toWei("500", "ether"), 0, {
         from: token_owner
       });
-      assert.equal(tx.logs[3].args._type.toNumber(), transferManagerKey, "SingleTradeVolumeRestrictionManager did not get deployed");
+      assert.equal(tx.logs[3].args._types[0].toNumber(), transferManagerKey, "SingleTradeVolumeRestrictionManager did not get deployed");
       assert.equal(
         web3.utils.toAscii(tx.logs[3].args._name)
         .replace(/\u0000/g, ''),
@@ -458,7 +451,7 @@ contract('SingleTradeVolumeRestrictionManager', accounts => {
       const tx = await I_SecurityToken.addModule(I_SingleTradeVolumeRestrictionManagerFactory.address, managerArgs, 0, 0, {
         from: token_owner
       });
-      assert.equal(tx.logs[2].args._type.toNumber(), transferManagerKey, "PercentageTransferManager doesn't get deployed");
+      assert.equal(tx.logs[2].args._types[0].toNumber(), transferManagerKey, "PercentageTransferManager doesn't get deployed");
       assert.equal(
         web3.utils.toAscii(tx.logs[2].args._name)
         .replace(/\u0000/g, ''),
@@ -485,7 +478,7 @@ contract('SingleTradeVolumeRestrictionManager', accounts => {
       const tx = await I_SecurityToken.addModule(I_SingleTradeVolumeRestrictionManagerFactory.address, managerArgs, 0, 0, {
         from: token_owner
       });
-      assert.equal(tx.logs[2].args._type.toNumber(), transferManagerKey, "PercentageTransferManager doesn't get deployed");
+      assert.equal(tx.logs[2].args._types[0].toNumber(), transferManagerKey, "PercentageTransferManager doesn't get deployed");
       assert.equal(
         web3.utils.toAscii(tx.logs[2].args._name)
         .replace(/\u0000/g, ''),

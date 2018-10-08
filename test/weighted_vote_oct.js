e7c04b20d671a138685f5d0e2283113b9ac7e939
comeonbuddy <comeonbuddy@gmail.com>
2018-10-08 11:11:16 +0200

    factory debug

--- test/weighted_vote_oct.js
+++ test/weighted_vote_oct.js
@@ -28,7 +28,7 @@ const Web3 = require('web3');
 const BigNumber = require('bignumber.js');
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545")) // Hardcoded development port
 
-contract('GeneralPermissionManager', accounts => {
+contract('WeightedVoteCheckpoint', accounts => {
 
     // Accounts Variable declaration
     let account_polymath;

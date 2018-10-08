a22772f90c435823a446e7971113225782cccad2
Mudit Gupta <guptamudit@ymail.com>
2018-10-08 15:16:11 +0530

    Testing

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ module.exports = {
     norpc: true,
     port: 8545,
     copyPackages: ['openzeppelin-solidity'],
-    testCommand: 'node ../node_modules/.bin/truffle test `find test/*.js ! -name a_poly_oracle.js -and ! -name s_v130_to_v140_upgrade.js` --network coverage',
+    testCommand: 'truffle test `find test/*.js ! -name a_poly_oracle.js -and ! -name s_v130_to_v140_upgrade.js` --network coverage',
     deepSkip: true,
     skipFiles: ['external', 'flat']
 };

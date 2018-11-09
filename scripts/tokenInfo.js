a6a3eb82d56978794fe51325a7932db4f46502ac
Victor Vicente <victor.g.vicente@gmail.com>
2018-11-09 13:12:35 -0300

    EtherscanAPI for tokenInfo.js script

--- scripts/tokenInfo.js
+++ scripts/tokenInfo.js
@@ -1,19 +1,18 @@
 const Web3 = require("web3");
-const fs = require("fs");
-const async = require("async");
-const path = require("path");
 const web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/"));
+var request = require('request-promise')
 
-const securityTokenRegistryABI = JSON.parse(require('fs').readFileSync('../build/contracts/SecurityTokenRegistry.json').toString()).abi;
-const securityTokenABI = JSON.parse(require('fs').readFileSync('../build/contracts/SecurityToken.json').toString()).abi;
-const generalTransferManagerABI = JSON.parse(require('fs').readFileSync('../build/contracts/GeneralTransferManager.json').toString()).abi;
-const securityTokenRegistryAddress = "0xEf58491224958d978fACF55D2120c55A24516B98";
-const securityTokenRegistry = new web3.eth.Contract(securityTokenRegistryABI, securityTokenRegistryAddress);
+const securityTokenABI = JSON.parse(require('fs').readFileSync('../CLI/data/SecurityToken1-4-0.json').toString()).abi;
+const generalTransferManagerABI = JSON.parse(require('fs').readFileSync('../CLI/data/GeneralTransferManager1-4-0.json').toString()).abi;
 
 async function getTokens() {
-    let strEvents = await web3.eth.getPastLogs({fromBlock:'0x5C5C18', address:securityTokenRegistry.address, topics: ["0x2510d802a0818e71139a7680a6388bcffcd3fa686e02a0f7319488c5bdb38fcb"]});
-    for (let i = 0; i < strEvents.length; i++) {
-        let tokenAddress = '0x' + strEvents[i].topics[1].slice(26,66)
+    const securityTokenRegistryAddress = "0xEf58491224958d978fACF55D2120c55A24516B98";
+    const securityTokenRegistryABI = await getABIfromEtherscan(securityTokenRegistryAddress);
+    const securityTokenRegistry = new web3.eth.Contract(securityTokenRegistryABI, securityTokenRegistryAddress);
+
+    let logs = await getLogsFromEtherscan(securityTokenRegistry.options.address, web3.utils.hexToNumber('0x5C5C18'), 'latest', 'LogNewSecurityToken(string,address,address)');
+    for (let i = 0; i < logs.length; i++) {
+        let tokenAddress = '0x' + logs[i].topics[1].slice(26,66)
         await getInfo(tokenAddress);
     }
 }
@@ -32,7 +31,7 @@ async function getInfo(tokenAddress) {
     console.log("Finished Issuer Minting: " + await token.methods.finishedIssuerMinting().call());
     console.log("Finished STO Minting: " + await token.methods.finishedSTOMinting().call());
     let gtmRes = await token.methods.modules(2, 0).call();
-    let gtmEvents = await web3.eth.getPastLogs({fromBlock:'0x5C5C18', address:gtmRes.moduleAddress});
+    let gtmEvents = await getLogsFromEtherscan(gtmRes.moduleAddress, web3.utils.hexToNumber('0x5C5C18'), 'latest', 'LogModifyWhitelist(address,uint256,address,uint256,uint256,uint256,bool)');
     console.log("Count of GeneralTransferManager Events: " + gtmEvents.length);
     console.log("Modules Attached (TransferManager):");
     await getModules(2, token);
@@ -63,6 +62,43 @@ async function getModules(type, token) {
         }
         index = index + 1;
     }
+}
+
+async function getLogsFromEtherscan(_address, _fromBlock, _toBlock, _eventSignature) {
+    let urlDomain = 'api';
+    const options = {
+        url: `https://${urlDomain}.etherscan.io/api`,
+        qs: {
+            module: 'logs',
+            action: 'getLogs',
+            fromBlock: _fromBlock,
+            toBlock: _toBlock,
+            address: _address,
+            topic0: web3.utils.sha3(_eventSignature),
+            apikey: 'THM9IUVC2DJJ6J5MTICDE6H1HGQK14X559'
+        },
+        method: 'GET',
+        json: true
+    };
+    let data = await request(options);
+    return data.result;
+}
+
+async function getABIfromEtherscan(_address) {
+    let urlDomain = 'api';
+    const options = {
+        url: `https://${urlDomain}.etherscan.io/api`,
+        qs: {
+            module: 'contract',
+            action: 'getabi',
+            address: _address,
+            apikey: 'THM9IUVC2DJJ6J5MTICDE6H1HGQK14X559'
+        },
+        method: 'GET',
+        json: true
+    };
+    let data = await request(options);
+    return JSON.parse(data.result);
 }
 
 getTokens();

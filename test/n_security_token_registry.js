418a99bc20264b98472827dda189ac656f5f6c28
satyam <satyam@secureblocks.io>
2018-11-10 21:42:30 +0530

    getTickersOwner fix

--- test/n_security_token_registry.js
+++ test/n_security_token_registry.js
@@ -993,7 +993,8 @@ contract("SecurityTokenRegistry", accounts => {
 
         it("Should get the tickers by owner", async () => {
             let tickersList = await I_STRProxied.getTickersByOwner.call(token_owner);
-            assert.equal(tickersList.length, 4);
+            console.log(tickersList);
+            assert.equal(tickersList.length, 3);
             let tickersListArray = await I_STRProxied.getTickersByOwner.call(account_temp);
             console.log(tickersListArray);
             assert.equal(tickersListArray.length, 3);

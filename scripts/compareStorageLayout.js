d473a2d30285c61e6c59bb7c7ebe767a71482cbf
satyam <satyam@secureblocks.io>
2018-11-27 14:02:33 +0530

    make script dynamic

--- scripts/compareStorageLayout.js
+++ scripts/compareStorageLayout.js
@@ -1,15 +1,52 @@
 const fs = require('fs');
-const logicContract = fs.readFileSync('./logic.sol', 'utf8');
-const proxyContract = fs.readFileSync('./proxy.sol', 'utf8');
 const _ = require('underscore');
 const solc = require('solc');
+const prompt = require('prompt');
+const path = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-let logicInput = {
-  'contracts': logicContract
-}
-let proxyInput = {
-  'contracts': proxyContract
-}
+prompt.start();
+
+prompt.get(['LogicContract', 'ProxyContract'], async(err, result) => {
+  
+  let logicContract;
+  let proxyContract;
+
+  const fileList = walkSync('./contracts', []);
+
+  let paths = findPath(result.LogicContract, result.ProxyContract, fileList);
+
+  if (paths.length == 2) {
+    
+    console.log("Contracts exists \n");
+    
+    await flatContracts(paths);
+
+    if (path.basename(paths[0]) === result.LogicContract) {
+      logicContract = fs.readFileSync(`./flat/${path.basename(paths[0])}`, 'utf8');
+    } else {
+      logicContract = fs.readFileSync(`./flat/${path.basename(paths[1])}`, 'utf8');
+    } 
+    if (path.basename(paths[0]) === result.ProxyContract) {
+      proxyContract = fs.readFileSync(`./flat/${path.basename(paths[0])}`, 'utf8');
+    } else {
+      proxyContract = fs.readFileSync(`./flat/${path.basename(paths[1])}`, 'utf8');
+    }
+
+    let logicInput = {
+      'contracts': logicContract
+    }
+    let proxyInput = {
+      'contracts': proxyContract
+    }
+
+    console.log(compareStorageLayouts(parseContract(logicInput), parseContract(proxyInput)));
+
+  } else {
+    console.log("Contracts doesn't exists");
+  }
+});
 
 function traverseAST(_input, _elements) {
   if(_input.children) {
@@ -67,5 +104,36 @@ function parseContract(input) {
   return orderedStateVariables;
 }
 
-// console.log(orderedStateVariables);
-console.log(compareStorageLayouts(parseContract(logicInput), parseContract(proxyInput)));
+var walkSync = function(dir, filelist) {
+  files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      filelist = walkSync(path.join(dir, file), filelist);
+    }
+    else {
+      filelist.push(path.join(dir, file));
+    }
+  });
+  return filelist;
+};
+
+var findPath = function(logicContractName, proxyContractName, fileList) {
+  let paths = new Array();
+  for (let i =0; i < fileList.length; i++) {
+    if ((logicContractName === path.basename(fileList[i]) || logicContractName === (path.basename(fileList[i])).split(".")[0]) || 
+      (proxyContractName === path.basename(fileList[i]) || proxyContractName === (path.basename(fileList[i])).split(".")[0])) {
+        paths.push(fileList[i]);
+    }
+  }
+  return paths;
+}
+
+async function flatContracts(_paths, _logic) {
+    let promises = new Array();
+    for (let i = 0; i< _paths.length; i++) {
+      promises.push(await exec(`./node_modules/.bin/sol-merger ${_paths[i]} ./flat`));
+    }
+    await Promise.all(promises);
+}
+

a72bcb220f83b439d7ee3eb5f76fe7c13fbdd842
Mudit Gupta <guptamudit@ymail.com>
2018-12-13 22:30:33 +0530

    Increased tolerance

--- contracts/libraries/DecimalMath.sol
+++ contracts/libraries/DecimalMath.sol
@@ -11,7 +11,7 @@ library DecimalMath {
      * @return uint256 Result of multiplication represented as (decimal * 10**DECIMALS)
      */
     function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = SafeMath.add(SafeMath.mul(x, y), (10 ** 18) / 2) / (10 ** 18);
+        z = SafeMath.mul(x, y) / (10 ** 18);
     }
 
     /**
@@ -19,7 +19,7 @@ library DecimalMath {
      * @return uint256 Result of division represented as (decimal * 10**DECIMALS)
      */
     function div(uint256 x, uint256 y) internal pure returns (uint256 z) {
-        z = SafeMath.add(SafeMath.mul(x, (10 ** 18)), y / 2) / y;
+        z = SafeMath.mul(x, (10 ** 18)) / y;
     }
 
 }

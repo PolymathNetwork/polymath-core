7aeb6cf7174ae38f827554ca22892057cafbec40
Mudit Gupta <guptamudit@ymail.com>
2018-12-20 11:47:31 +0530

    cappedsto init fn fixed

--- contracts/modules/STO/CappedSTO.sol
+++ contracts/modules/STO/CappedSTO.sol
@@ -81,7 +81,7 @@ contract CappedSTO is STO, ReentrancyGuard {
      * @notice This function returns the signature of configure function
      */
     function getInitFunction() public pure returns(bytes4) {
-        return bytes4(keccak256("configure(uint256,uint256,uint256,uint256,uint8[],address payable)"));
+        return bytes4(keccak256("configure(uint256,uint256,uint256,uint256,uint8[],address)"));
     }
 
     /**

a9a7b221377054f2f1cef79b6afb43962bebab4c
Mudit Gupta <guptamudit@ymail.com>
2019-01-08 19:52:33 +0530

    triggering travis

--- contracts/mocks/SecurityTokenRegistryMock.sol
+++ contracts/mocks/SecurityTokenRegistryMock.sol
@@ -7,7 +7,7 @@ import "../SecurityTokenRegistry.sol";
  */
 contract SecurityTokenRegistryMock is SecurityTokenRegistry {
     /// @notice It is a dummy function
-    /// Alert! Alert! Do not use it for the mainnet release
+    /// Alert! Alert! Do NOT use it for the mainnet release
     function changeTheDeployedAddress(string memory _ticker, address _newSecurityTokenAddress) public {
         set(Encoder.getKey("tickerToSecurityToken", _ticker), _newSecurityTokenAddress);
     }

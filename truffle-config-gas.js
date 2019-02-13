2ff6d5848d5275a7a74c9b7defc10ed3a816b88d
Mudit Gupta <guptamudit@ymail.com>
2019-02-13 18:31:00 +0530

    gas usage script

--- truffle-config-gas.js
+++ truffle-config-gas.js
@@ -69,6 +69,13 @@ module.exports = {
     }
   },
   mocha: {
-    enableTimeouts: false
+    enableTimeouts: false,
+    reporter: 'eth-gas-reporter',
+    reporterOptions : {
+      currency: 'USD',
+      gasPrice: 5,
+      onlyCalledMethods: true,
+      showTimeSpent: true
+    }
   }
 };

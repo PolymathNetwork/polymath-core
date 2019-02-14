5d1274e477c4693d5ecc4919e176ce6d36a552f6
satyam <satyam@secureblocks.io>
2019-02-14 15:25:35 +0530

    add table

--- scripts/calculateSize.js
+++ scripts/calculateSize.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const exec = require('child_process').execSync;
 const chalk = require('chalk');
+const { table }  = require("table");
 
 async function readFiles() {
     if (fs.existsSync("./build/contracts/")) {
@@ -17,16 +18,28 @@ async function printSize() {
     let files = await readFiles();
     console.log(`NOTE- Maximum size of contracts allowed to deloyed on the Ethereum mainnet is 24 KB(EIP170)`);
     console.log(`---- Size of the contracts ----`);
+    let contracts = new Array();
+    let size = new Array();
     files.forEach(item => {
         let content = JSON.parse(fs.readFileSync(`./build/contracts/${item}`).toString()).deployedBytecode;
         let sizeInKB = content.toString().length / 2 / 1024;
+        size.push(sizeInKB);
         if (sizeInKB > 24)
-            console.log(chalk.red(`${path.basename(item, ".json")} -  ${sizeInKB} KB`));
+            contracts.push(chalk.red(path.basename(item, ".json")));
         else if (sizeInKB > 20)
-            console.log(chalk.yellow(`${path.basename(item, ".json")} -  ${sizeInKB} KB`));
+            contracts.push(chalk.yellow(path.basename(item, ".json")));
         else
-            console.log(chalk.green(`${path.basename(item, ".json")} -  ${sizeInKB} KB`));
+            contracts.push(chalk.green(path.basename(item, ".json")));
     });
+    let dataTable = [['Contracts', 'Size in KB']];
+    for (let i = 0; i < contracts.length; i++) {
+        dataTable.push([
+            contracts[i],
+            size[i]
+        ]);
+    }
+    console.log();
+    console.log(table(dataTable));
 }
 
 printSize();

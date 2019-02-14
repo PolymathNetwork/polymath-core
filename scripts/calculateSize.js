ba5c75aa01a60c7d809a06820131b4eb111b323f
Mudit Gupta <guptamudit@ymail.com>
2019-02-14 15:32:37 +0530

    opitmized table

--- scripts/calculateSize.js
+++ scripts/calculateSize.js
@@ -18,26 +18,17 @@ async function printSize() {
     let files = await readFiles();
     console.log(`NOTE- Maximum size of contracts allowed to deloyed on the Ethereum mainnet is 24 KB(EIP170)`);
     console.log(`---- Size of the contracts ----`);
-    let contracts = new Array();
-    let size = new Array();
+    let dataTable = [['Contracts', 'Size in KB']];
     files.forEach(item => {
         let content = JSON.parse(fs.readFileSync(`./build/contracts/${item}`).toString()).deployedBytecode;
         let sizeInKB = content.toString().length / 2 / 1024;
-        size.push(sizeInKB);
         if (sizeInKB > 24)
-            contracts.push(chalk.red(path.basename(item, ".json")));
+            dataTable.push([chalk.red(path.basename(item, ".json")),chalk.red(sizeInKB)]);
         else if (sizeInKB > 20)
-            contracts.push(chalk.yellow(path.basename(item, ".json")));
+            dataTable.push([chalk.yellow(path.basename(item, ".json")),chalk.yellow(sizeInKB)]);
         else
-            contracts.push(chalk.green(path.basename(item, ".json")));
+            dataTable.push([chalk.green(path.basename(item, ".json")),chalk.green(sizeInKB)]);
     });
-    let dataTable = [['Contracts', 'Size in KB']];
-    for (let i = 0; i < contracts.length; i++) {
-        dataTable.push([
-            contracts[i],
-            size[i]
-        ]);
-    }
     console.log(table(dataTable));
 }
 

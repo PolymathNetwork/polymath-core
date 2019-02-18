c898e636b2426a8d1c582f488e8dac6499bf0c34
satyam <satyam@secureblocks.io>
2019-02-18 10:44:42 +0530

    add minor comment

--- contracts/modules/ModuleStorage.sol
+++ contracts/modules/ModuleStorage.sol
@@ -13,7 +13,7 @@ contract ModuleStorage {
 
     bytes32 public constant FEE_ADMIN = "FEE_ADMIN";
 
-    bytes32 internal constant TREASURY = 0xaae8817359f3dcb67d050f44f3e49f982e0359d90ca4b5f18569926304aaece6;
+    bytes32 internal constant TREASURY = 0xaae8817359f3dcb67d050f44f3e49f982e0359d90ca4b5f18569926304aaece6; // keccak256(abi.encodePacked("TREASURY_WALLET"))
 
     IERC20 public polyToken;
 

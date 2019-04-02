95580a0c851aa1112dfecf031b85e3cbefcd47f1
Mudit Gupta <guptamudit@ymail.com>
2019-04-02 14:30:46 +0530

    Added clash checker script

---
+++ scripts/clashCheck.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const exec = require('child_process').execSync;
+const chalk = require('chalk');
+const Web3 = require('web3');
+
+async function readFiles() {
+    if (fs.existsSync("./build/contracts/")) {
+        return fs.readdirSync("./build/contracts/");
+    } else {
+        console.log(chalk.yellow('Compiling contracts. This may take a while, please wait.'));
+        exec('truffle compile');
+        return fs.readdirSync("./build/contracts/");
+    }
+}
+
+async function checkClashes() {
+    let files = await readFiles();
+    let contractFunctions = new Set();
+    let functionSelectors = new Map();
+    files.forEach(item => {
+        let ABI = JSON.parse(fs.readFileSync(`./build/contracts/${item}`).toString()).abi;
+        ABI.forEach(element => {
+            if (element['type'] == 'function') {
+                let functionSig = element['name'] + '(';
+                element['inputs'].forEach(input => {
+                    functionSig = functionSig + input['type'] + ',';
+                });
+                if(functionSig[functionSig.length - 1] == ',')
+                    functionSig = functionSig.slice(0, -1) + ')';
+                else
+                    functionSig = functionSig + ')';
+                contractFunctions.add(functionSig);
+            }
+        });
+    });
+    contractFunctions.forEach(functionSig => {
+        let fnSelector = Web3.utils.sha3(functionSig).slice(0, 10);
+        if(functionSelectors.has(fnSelector)) {
+            console.log(chalk.red('Function selector clash found!', functionSelectors.get(fnSelector), 'and', functionSig, 'have the same function selector:', fnSelector));
+            functionSelectors.set(fnSelector, functionSelectors.get(fnSelector) + ', ' + functionSig);
+        } else {
+            functionSelectors.set(fnSelector, functionSig);
+        }
+    });
+    console.log(chalk.green("Clash check finished. If you don't see any red error messages, you are good to go."));
+}
+
+checkClashes();

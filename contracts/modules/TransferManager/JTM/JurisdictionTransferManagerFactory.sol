609cd56de238506c9ba9dd109c4eb4591dc4bf28
Adam Dossa <adam.dossa@gmail.com>
2019-06-03 08:38:16 -0400

    Wip

---
+++ contracts/modules/TransferManager/JTM/JurisdictionTransferManagerFactory.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.5.0;
+
+import "./GeneralTransferManagerProxy.sol";
+import "../../UpgradableModuleFactory.sol";
+
+/**
+ * @title Factory for deploying GeneralTransferManager module
+ */
+contract GeneralTransferManagerFactory is UpgradableModuleFactory {
+
+    /**
+     * @notice Constructor
+     * @param _setupCost Setup cost of the module
+     * @param _usageCost Usage cost of the module
+     * @param _logicContract Contract address that contains the logic related to `description`
+     * @param _polymathRegistry Address of the Polymath registry
+     * @param _isCostInPoly true = cost in Poly, false = USD
+     */
+    constructor (
+        uint256 _setupCost,
+        uint256 _usageCost,
+        address _logicContract,
+        address _polymathRegistry,
+        bool _isCostInPoly
+    )
+        public
+        UpgradableModuleFactory("3.0.0", _setupCost, _usageCost, _logicContract, _polymathRegistry, _isCostInPoly)
+    {
+        name = "GeneralTransferManager";
+        title = "General Transfer Manager";
+        description = "Manage transfers using a time based whitelist";
+        typesData.push(2);
+        typesData.push(6);
+        tagsData.push("General");
+        tagsData.push("Transfer Restriction");
+        compatibleSTVersionRange["lowerBound"] = VersionUtils.pack(uint8(3), uint8(0), uint8(0));
+        compatibleSTVersionRange["upperBound"] = VersionUtils.pack(uint8(3), uint8(0), uint8(0));
+    }
+
+    /**
+     * @notice Used to launch the Module with the help of factory
+     * @return address Contract address of the Module
+     */
+    function deploy(
+        bytes calldata _data
+    )
+        external
+        returns(address)
+    {
+        address generalTransferManager = address(new GeneralTransferManagerProxy(logicContracts[latestUpgrade].version, msg.sender, IPolymathRegistry(polymathRegistry).getAddress("PolyToken"), logicContracts[latestUpgrade].logicContract));
+        _initializeModule(generalTransferManager, _data);
+        return generalTransferManager;
+    }
+
+}

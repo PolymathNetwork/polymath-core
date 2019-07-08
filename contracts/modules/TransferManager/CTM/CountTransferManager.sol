c2e643463d97d7dc19ede910d60ffd19c1ae6228
Mudit Gupta <guptamudit@ymail.com>
2019-07-08 20:45:17 +0530

    Fixed CTM verifyTransfer bug (#736)
    * Fixed CTM canTransferBug
    
    * Added test case
    
    * Added comment for devs

--- contracts/modules/TransferManager/CTM/CountTransferManager.sol
+++ contracts/modules/TransferManager/CTM/CountTransferManager.sol
@@ -33,12 +33,16 @@ contract CountTransferManager is CountTransferManagerStorage, TransferManager {
         external
         returns(Result)
     {
-        (Result success, ) = _verifyTransfer(_from, _to, _amount);
+        (Result success, ) = _verifyTransfer(_from, _to, _amount, securityToken.holderCount());
         return success;
     }
 
     /**
      * @notice Used to verify the transfer transaction and prevent a transfer if it passes the allowed amount of token holders
+     * @dev module.verifyTransfer is called by SecToken.canTransfer and does not receive the updated holderCount therefore
+     *      verifyTransfer has to manually account for pot. tokenholder changes (by mimicking TokenLib.adjustInvestorCount).
+     *      module.executeTransfer is called by SecToken.transfer|issue|others and receives an updated holderCount 
+     *      as sectoken calls TokenLib.adjustInvestorCount before executeTransfer.
      * @param _from Address of the sender
      * @param _to Address of the receiver
      * @param _amount Amount to send
@@ -53,20 +57,33 @@ contract CountTransferManager is CountTransferManagerStorage, TransferManager {
         view
         returns(Result, bytes32)
     {
-        return _verifyTransfer(_from, _to, _amount);
+        uint256 holderCount = securityToken.holderCount();
+        if (_amount != 0 && _from != _to) {
+            // Check whether receiver is a new token holder
+            if (_to != address(0) && securityToken.balanceOf(_to) == 0) {
+                holderCount++;
+            }
+            // Check whether sender is moving all of their tokens
+            if (_amount == securityToken.balanceOf(_from)) {
+                holderCount--;
+            }
+        }
+
+        return _verifyTransfer(_from, _to, _amount, holderCount);
     }
 
     function _verifyTransfer(
         address _from,
         address _to,
-        uint256 _amount
+        uint256 _amount,
+        uint256 _holderCount
     )
         internal
         view
         returns(Result, bytes32)
     {
         if (!paused) {
-            if (maxHolderCount < securityToken.holderCount()) {
+            if (maxHolderCount < _holderCount) {
                 // Allow transfers to existing maxHolders
                 if (securityToken.balanceOf(_to) != 0 || securityToken.balanceOf(_from) == _amount) {
                     return (Result.NA, bytes32(0));

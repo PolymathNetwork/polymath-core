91acc28658b37309b1a0873b37e8c377f482fe73
Remon Nashid <remon.sherin@gmail.com>
2019-07-16 09:40:44 -0400

    tryCatch to check if error contains a string

--- test/helpers/exceptions.js
+++ test/helpers/exceptions.js
@@ -1,29 +1,19 @@
-const PREFIX = "VM Exception while processing transaction: ";
-const PREFIX2 = "Returned error: VM Exception while processing transaction: ";
-
-async function tryCatch(promise, message) {
+async function tryCatch(promise, expectedError) {
     try {
         await promise;
-        throw null;
     } catch (error) {
-        assert(error, "Expected an error but did not get one");
-        try {
-            assert(
-                error.message.startsWith(PREFIX + message),
-                "Expected an error starting with '" + PREFIX + message + "' but got '" + error.message + "' instead"
-            );
-        } catch (err) {
-            assert(
-                error.message.startsWith(PREFIX2 + message),
-                "Expected an error starting with '" + PREFIX + message + "' but got '" + error.message + "' instead"
-            );
+        if (error.message.indexOf(expectedError) === -1) {
+        throw Error(`Wrong failure type, expected '${expectedError}' and got '${error.message}'`);
         }
+        return;
     }
+
+    throw Error('Expected failure not received');
 }
 
 module.exports = {
-    catchRevert: async function(promise, message) {
-        await tryCatch(promise, message ? `revert ${message}` : "revert");
+    catchRevert: async function(promise, message = 'revert') {
+        await tryCatch(promise, message);
     },
     catchPermission: async function(promise) {
         await tryCatch(promise, "revert Permission check failed");
